3da9262282adf6d96ccf361bdbbc88b1356f12a9
ahmetson <admin@blocklords.io>
2021-07-27 06:07:02 +0700

    Create carpet ad man hero scripts

---
+++ Assets/Scripts/TestMainCharacter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMainCharacter : MonoBehaviour
+{
+    public bool startPoint = false;
+
+    
+}
