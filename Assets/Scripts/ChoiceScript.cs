aa9bf54d4c171f1287a1efa6d14e4be2fb7f54fd
yeqwer <andreymixler@gmail.com>
2023-02-15 17:12:59 +0700

    add change crystal material

--- Assets/Scripts/ChoiceScript.cs
+++ Assets/Scripts/ChoiceScript.cs
@@ -1,41 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
+
 
 public class ChoiceScript : MonoBehaviour
 {
     public Camera cam;
-    public Renderer rend;
+    public Renderer handCrystal;
+    
     public LayerMask layerMask;
     public List<Material> materials;
-
-
-    void Start()
-    {
-      //  cam = GetComponent<Camera>();
-      //  rend = GetComponent<Renderer>();
-    }
+    private Material mat;
 
     public void Update()
     {
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
         RaycastHit hit;
+        
 
         if (Physics.Raycast(ray, out hit, 4f, layerMask))
         {
             print("Im looking at " + hit.transform.name);
             Debug.DrawLine(ray.origin, hit.point);
-            
-            if (Input.GetMouseButton(0)) 
-            {
-                if (hit.transform.name == "outsideYellow")
-                    rend.material = materials[4];
-                if (hit.transform.name == "outsideBlue")
-                    rend.material = materials[0];
-                if (hit.transform.name == "outsideGreen")
-                    rend.material = materials[2];
 
+            if (Input.GetMouseButtonDown(0)) {
+
+
+                /*
+                if (hit.collider.GetComponentInParent<Renderer>().material == materials[4]) //yellow
+                {
+                    hit.collider.GetComponentInParent<Renderer>().material = mat;
+                    mat = materials[4];
+                    handCrystal.material = materials[4];
+                } 
+                else if (hit.collider.GetComponentInParent<Renderer>().material == materials[0]) //blue
+                {
+                    hit.collider.GetComponentInParent<Renderer>().material = mat;
+                    mat = materials[0];
+                    handCrystal.material = materials[0];
+                }
+                else if (hit.collider.GetComponentInParent<Renderer>().material == materials[2]) //green
+                {
+                    hit.collider.GetComponentInParent<Renderer>().material = mat;
+                    mat = materials[2];
+                    handCrystal.material = materials[2];
+                }
+                else if (hit.collider.GetComponentInParent<Renderer>().material == materials[1]) //empty
+                {
+                    hit.collider.GetComponentInParent<Renderer>().material = mat;
+                    mat = materials[1];
+                    handCrystal.material = materials[1];
+                }
+               */ 
+
+                mat = handCrystal.material;
+                handCrystal.material = hit.collider.GetComponentInParent<Renderer>().material;
+                hit.collider.GetComponentInParent<Renderer>().material = mat;
+
+                
+
+                /*   hit.collider.GetComponentInParent<Renderer>().material = mat;
+
+                   if (hit.transform.name == "insideYellow")
+                   {
+
+                       mat = hit.collider.GetComponentInParent<Renderer>().material;
+                       hit.collider.GetComponentInParent<Renderer>().material = handCrystal.material;
+                       handCrystal.material = mat;
+
+                       hit.transform.name = "insideEmpty";
+                   }
+                   else if (hit.transform.name == "insideBlue")
+                   {
+
+                       mat = hit.collider.GetComponentInParent<Renderer>().material;
+                       hit.collider.GetComponentInParent<Renderer>().material = handCrystal.material;
+                       handCrystal.material = mat;
+
+                       hit.transform.name = "insideEmpty";
+                   }
+                   else if (hit.transform.name == "insideGreen")
+                   {
+
+                       mat = hit.collider.GetComponentInParent<Renderer>().material;
+
+                       handCrystal.material = mat;
+
+                       hit.transform.name = "insideEmpty";
+                   }
+                   else if (hit.transform.name == "insideEmpty")
+                   {
+                       hit.collider.GetComponentInParent<Renderer>().material = mat;
+
+                       //Debug.Log(handCrystal.material);
+
+
+                       if (mat == materials[4]) 
+                       {
+                           hit.transform.name = "insideYellow";              
+                       }
+                       else if (mat == materials[0])
+                       {
+                           hit.transform.name = "insideBlue";
+                           Debug.Log("done blue");
+                       }
+                       else if (mat == materials[2])
+                       {
+                           hit.transform.name = "insideGreen";     
+                       }
+
+                       mat = materials[1];
+                       handCrystal.material = materials[1];
+
+
+                   } */
             }
         }
         else
---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    [Header("Pickup Settings")]
+    [SerializeField] Transform holdArea;
+    private GameObject heldObj;
+    private Rigidbody heldObjRB;
+
+    [Header("Physics Parameters")]
+    [SerializeField] private float pickupRange = 5.0f;
+    [SerializeField] private float pickupForce = 150.0f;
+     
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (heldObj == null) 
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                {
+
+                }
+            }
+        }   
+    }
+
+}
