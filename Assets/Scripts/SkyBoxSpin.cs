e9c5c12e71d41d357cebb21e1f9fa32a4ba5d52b
yeqwer <andreymixler@gmail.com>
2023-03-11 05:31:28 +0700

    remove extra assets and add VFX

--- Assets/Scripts/SkyBoxSpin.cs
+++ Assets/Scripts/SkyBoxSpin.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SkyBoxSpin : MonoBehaviour
 {
     private float rotateSpeed = 1.2f;
-  
+    public Light globalLignt;
+
     void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed);
+        globalLignt.transform.RotateAround(Vector3.zero, Vector3.down, 0.01f);
     }
 }
